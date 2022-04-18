b402665ff56d09d41c429d35324cd39ccf507a7a
Dennis <den.goehlert@gmail.com>
2022-04-19 01:20:16 +0200

    Fixed: Removed "em(...)" calls around success messages
    Signed-off-by: Dennis <den.goehlert@gmail.com>

--- src/replies.py
+++ src/replies.py
@@ -89,8 +89,8 @@ def smiley(n):
 format_strs = {
 	types.CUSTOM: "{text}",
 	types.SUCCESS: "☑",
-	types.SUCCESS_DELETE: em("☑ A message by <b>{id}</b> has been deleted"),
-    types.SUCCESS_DELETEALL: em("☑ All {count} messages by <b>{id}</b> have been deleted"),
+	types.SUCCESS_DELETE: "☑ A message by <b>{id}</b> has been deleted",
+    types.SUCCESS_DELETEALL: "☑ All {count} messages by <b>{id}</b> have been deleted",
 	types.BOOLEAN_CONFIG: lambda enabled, **_:
 		"<b>{description!x}</b>: " + (enabled and "enabled" or "disabled"),
 
