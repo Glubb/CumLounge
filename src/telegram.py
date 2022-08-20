82bbc9e927be8e8fb988329b8dad7ed55e1484ef
Dennis <den.goehlert@gmail.com>
2022-08-21 00:28:48 +0200

    Changed: Inverted logic for final release
    Signed-off-by: Dennis <den.goehlert@gmail.com>

--- src/telegram.py
+++ src/telegram.py
@@ -378,7 +378,7 @@ def resend_message(chat_id, ev, reply_to=None, force_caption: FormattedMessage=N
 	# We need the full Chat object here, because some properties are not available in the ev.chat trait
 	tchat = bot.get_chat(ev.from_user.id)
 	# Check if the message is either voice or video and if the user has disabled them
-	if (ev.content_type in ("video_note", "voice")) and not tchat.has_restricted_voice_and_video_messages:
+	if (ev.content_type in ("video_note", "voice")) and tchat.has_restricted_voice_and_video_messages:
 		return bot.send_message(chat_id, rp.formatForTelegram(rp.Reply(rp.types.ERR_VOICE_AND_VIDEO_PRIVACY_RESTRICTION)), parse_mode="HTML")
 
 	# re-send message based on content type
